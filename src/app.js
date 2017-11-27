5d5614e5f90c1af949a10e13b395fc677071c59a
Shaunna <skwiens@gmail.com>
2017-11-27 15:41:11 -0800

    initial song setup

--- src/app.js
+++ src/app.js
@@ -6,8 +6,31 @@ import _ from 'underscore';
 import './css/foundation.css';
 import './css/style.css';
 
+const songData = [
+  {
+    title: "Drop It Like It's Hot",
+    year: 2004,
+    artist: "Snoop Dog"
+  },
+  {
+    title: "Concierto de Arenjuez",
+    year: 1979,
+    artist: "Paco de Lucia"
+  },
+  {
+    title: "Macarena",
+    year: 1993,
+    artist: "Los Del Rio"
+  },
+  {
+    title: "Electric Boogie",
+    year: 1990,
+    artist: "Marcia Griffiths"
+  },
+  ];
+
 console.log('it loaded!');
 
 $(document).ready( () => {
-  
+
 });
